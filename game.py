d17ad763f199b26c4732206e5dfa03d8d157b946
Ibrahim Dianov <dianovibragim@gmail.com>
2025-12-03 16:54:50 +0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,59 @@
+import random
+# Rock Paper Scissors ASCII Art
+
+# Rock
+rock = ("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+
+# Paper
+paper = ("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+# Scissors
+scissors = ("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+
+
+game_images = [rock,paper,scissors]
+
+user_choice = int(input("What do you choouse? Type 0 for Rock,1 for Paper or 2 for scissors.")) 
+
+if user_choice < 0 and user_choice > 2:
+    print("You type an invalid number. You lose!")
+else:
+    computer_choice = random.randint(0,2)
+
+print("You chose:")
+print(game_images[user_choice])
+
+print("Computer choice:")
+print(game_images[computer_choice])
+
+if user_choice == computer_choice:
+    print("It's a draw!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 0:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win")
+else:
+    print("You lose!")
